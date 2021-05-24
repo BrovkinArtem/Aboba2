0515067dac261697348318077db6661f2fe45255
BrovkinArtem <78733604+BrovkinArtem@users.noreply.github.com>
2021-05-24 03:13:41 +0300

    Update common.h

--- app/common.h
+++ app/common.h
@@ -2,7 +2,7 @@
  * common.h -- прототипы функций реализации команд редактора
  * 
  * Copyright (c) 2021, Alexander Borodin <aborod@petrsu.ru> 
- *                     Kvist Tatyana <kvist_tatyana@mail.ru>
+ *                     
  *
  * This code is licensed under a MIT-style license.
  */
